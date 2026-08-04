05df2955e98f581fdb6b9125408e6f802b01849f
Glêsio Santos <glesioss@gmail.com>
2026-08-04 11:08:00 -0300

    configurando banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseOptions } from './config/datasource';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env"
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => databaseOptions()
     })
   ],
   controllers: [AppController],
