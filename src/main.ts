d7d2daaa16c9306080c45aca234c9de9ce3ecefa
devluisdasilva01-lang <dev.luisdasilva.01@gmail.com>
2026-08-05 09:51:55 -0300

    adicionando validação dos resources

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app) //swagger
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
