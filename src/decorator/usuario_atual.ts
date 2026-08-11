d6f5e00ec11b7a450dea3b17c423a53dd5b61fef
devluisdasilva01-lang <dev.luisdasilva.01@gmail.com>
2026-08-11 02:10:56 -0300

    protegendo rota de perfil com jwt

--- src/decorator/usuario_atual.ts
+++ src/decorator/usuario_atual.ts
@@ -2,11 +2,11 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { RequisicaoAutenticada } from "src/guards/auth_guard";
 
 export const UsuarioAtual =  createParamDecorator(
-    (propiedade: string | undefined, context: ExecutionContext) => {
+    (propriedade: string | undefined, context: ExecutionContext) => {
         const requisicao = context.switchToHttp().getRequest<RequisicaoAutenticada>()
 
         const usuario = requisicao.usuario
 
-        return propiedade ? usuario?.[propiedade as keyof typeof usuario] : usuario 
+        return propriedade ? usuario?.[propriedade as keyof typeof usuario] : usuario 
     }
 )
