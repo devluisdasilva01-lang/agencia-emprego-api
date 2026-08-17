2f981b525c24b6736538031c24e10ef18de37125
devluisdasilva01-lang <dev.luisdasilva.01@gmail.com>
2026-08-16 23:40:59 -0300

    modelando agencia e configurando persistencia

--- src/modules/agencia/agencia.module.ts
+++ src/modules/agencia/agencia.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgenciaController } from './agencia.controller';
 import { AgenciaService } from './agencia.service';
+import { AgenciaModel } from './agencia.model';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AgenciaModel])],
   controllers: [AgenciaController],
   providers: [AgenciaService]
 })
